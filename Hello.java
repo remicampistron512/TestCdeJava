9d8c250b2a9f0ed3e2275b2031a6249a66aa0a4a
Rémi Campistron <remi.campistron@gmail.com>
2025-10-27 13:24:55 +0100

    Invite de commande nom/prenom et affichage

--- Hello.java
+++ Hello.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Bonjour et bienvenue dans mon programme java");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Entrez votre nom");
+		String name = scan.nextLine();
+		System.out.println("Entrez votre prenom");
+		String firstname = scan.nextLine();
+		System.out.println("Bienvenue "+ firstname + " " + name);
+		
+		
 	}
 }
 
